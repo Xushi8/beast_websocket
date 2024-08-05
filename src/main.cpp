a4299f2c252b1417996fcf2438a48d07f9f80934
uuufa <trump@niubi.com>
2024-08-05 23:49:25 +0800

    signal set

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <beast_websocket/log.hpp>
 #include <beast_websocket/websocket_server.hpp>
+#include <boost/asio/signal_set.hpp>
 #include <fmt/format.h>
 using fmt::print;
 
@@ -8,8 +9,16 @@ int main()
 	beast_websocket::set_default_log("beast_websocket", "logs/beast_websocket.txt");
 
 	boost::asio::io_context ctx;
+
+	boost::asio::signal_set signals(ctx, SIGINT, SIGTERM);
+	signals.async_wait([&](auto, auto)
+		{
+			ctx.stop();
+		});
+	
 	beast_websocket::websocket_server server(ctx, 8888);
 	server.start_accept();
+	
 	ctx.run();
 	return 0;
 }
