774bb0a42915d1db4306ffd3f0d05921a49bb01e
uuufa <trump@niubi.com>
2024-08-06 23:17:19 +0800

    多线程测试

--- src/network/src/websocket_server.cpp
+++ src/network/src/websocket_server.cpp
@@ -13,6 +13,7 @@ websocket_server::websocket_server(boost::asio::io_context& ctx, uint16_t port) 
 
 void websocket_server::start_accept()
 {
+	// auto connection_ptr = std::make_shared<connection>(m_ctx);
 	auto connection_ptr = std::make_shared<connection>(io_context_pool::instance().get_context());
 	m_acceptor.async_accept(connection_ptr->get_socket(), [this, connection_ptr](boost::system::error_code ec)
 		{
